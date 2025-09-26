6d8f46c5a1a0c46ef47735f252f6d50a57ffaee7
Easley <nevermore.wd@me.com>
2025-09-26 17:48:42 +0800

    some improvements

--- src/AsyncNavigation/RegionNavigationService.cs
+++ src/AsyncNavigation/RegionNavigationService.cs
@@ -7,13 +7,12 @@ namespace AsyncNavigation;
 
 internal sealed class RegionNavigationService<T> : IRegionNavigationService<T> where T : IRegionPresenter
 {
-    private readonly AsyncConcurrentItem<IView> Current = new();
     private readonly IViewManager _viewCacheManager;
     private readonly IRegionIndicatorManager _regionIndicatorManager;
     private readonly INavigationJobScheduler _navigationJobScheduler;
     private readonly IRegionPresenter _regionPresenter;
     private readonly RequestUnloadHandler _unloadHandler;
-
+    private volatile IView? _current;
     public RegionNavigationService(T regionPresenter, IServiceProvider serviceProvider)
     {
         _regionPresenter = regionPresenter;
@@ -22,7 +21,11 @@ internal sealed class RegionNavigationService<T> : IRegionNavigationService<T> w
         _regionIndicatorManager = serviceProvider.GetRequiredService<IRegionIndicatorManager>();
         _unloadHandler = new RequestUnloadHandler(_regionPresenter, _viewCacheManager);
     }
-
+    internal IView? CurrentView
+    {
+        get => _current;
+        set => _current = value;
+    }
     public async Task<NavigationResult> RequestNavigateAsync(NavigationContext navigationContext)
     {
         var stopwatch = Stopwatch.StartNew();
@@ -116,9 +119,9 @@ internal sealed class RegionNavigationService<T> : IRegionNavigationService<T> w
 
     private async Task HandleBeforeNavigationAsync(NavigationContext navigationContext)
     {
-        if (Current.TryTakeData(out IView? currentView))
+        if (CurrentView is not null)
         {
-            var currentAware = (currentView!.DataContext as INavigationAware)!;
+            var currentAware = (CurrentView.DataContext as INavigationAware)!;
             navigationContext.CancellationToken.ThrowIfCancellationRequested();
             await currentAware.OnNavigatedFromAsync(navigationContext);
             if (_regionPresenter.IsSinglePageRegion)
@@ -137,7 +140,7 @@ internal sealed class RegionNavigationService<T> : IRegionNavigationService<T> w
             _unloadHandler.Attach(aware, navigationContext);
             await aware.OnNavigatedToAsync(navigationContext);
             navigationContext.CancellationToken.ThrowIfCancellationRequested();
-            Current.SetData(view);
+            CurrentView = view;
         }
     }
     private static async Task ExecuteStepsAsync(IEnumerable<Func<NavigationContext, Task>> steps, NavigationContext navigationContext)
