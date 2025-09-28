95037c60490995e188b618a2fd4234903cff8fa9
Easley <nevermore.wd@me.com>
2025-09-28 17:58:20 +0800

    unit test

---
+++ tests/AsyncNavigation.E2E.Tests/App.axaml.cs
@@ -0,0 +1,29 @@
+﻿using AsyncNavigation.Core;
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AsyncNavigation.E2E.Tests;
+
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+
+        NavigationOptions navigationOptions = new()
+        {
+            NavigationJobStrategy = NavigationJobStrategy.CancelCurrent
+        };
+
+        var services = new ServiceCollection();
+        services.AddNavigationSupport(navigationOptions);
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ tests/AsyncNavigation.Tests/ServiceFixture.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AsyncNavigation.Tests;
+
+public class ServiceFixture : IDisposable
+{
+    public IServiceProvider ServiceProvider { get; }
+
+    public ServiceFixture()
+    {
+        ServiceCollection serviceDescriptors = new();
+        serviceDescriptors.RegisterNavigationFramework();
+        ServiceProvider = serviceDescriptors.BuildServiceProvider();
+    }
+
+    public void Dispose()
+    {
+        if (ServiceProvider is IDisposable d) d.Dispose();
+    }
+}
