9a4df5eac0cde2fcd542e487fe7aede8a437a55f
Easley <nevermore.wd@me.com>
2025-10-04 00:13:33 +0800

    wpf gc issue

--- src/AsyncNavigation/WeakRegionControlAccessor.cs
+++ src/AsyncNavigation/WeakRegionControlAccessor.cs
@@ -6,11 +6,13 @@ namespace AsyncNavigation;
 public sealed class WeakRegionControlAccessor<TControl> : IRegionControlAccessor<TControl> where TControl : class
 {
     private readonly WeakReference<TControl> _controlRef;
+    //private readonly TControl _controlRef;
     private volatile bool _alive = true;
     public WeakRegionControlAccessor(TControl control)
     {
         ArgumentNullException.ThrowIfNull(control);
         _controlRef = new WeakReference<TControl>(control);
+        //_controlRef = control;
     }
 
     public void ExecuteOn(Action<TControl> action)
@@ -37,7 +39,9 @@ public sealed class WeakRegionControlAccessor<TControl> : IRegionControlAccessor
             control = null;
             return false;
         }
+        //control = _controlRef;
         _alive = _controlRef.TryGetTarget(out control);
+        //_alive = true;
         return _alive;
     }
 }
