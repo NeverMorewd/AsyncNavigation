13f9599d79f5c852e14c1ff24db41e5b9df979f2
nevermore <nevermore.wd@me.com>
2025-10-20 18:49:04 +0800

    RegisterNavigation (#17)
    RegisterNavigation
    RegisterDialog
    
    (cherry picked from commit 33cc07cca9848b3de3d8dd37d50c9a30248da8e5)

---
+++ tests/AsyncNavigation.Tests/Mocks/AnotherTestNavigationAware.cs
@@ -0,0 +1,34 @@
+﻿using AsyncNavigation.Abstractions;
+using AsyncNavigation.Core;
+
+namespace AsyncNavigation.Tests.Mocks;
+
+public class AnotherTestNavigationAware : INavigationAware
+{
+    public event AsyncEventHandler<AsyncEventArgs>? AsyncRequestUnloadEvent;
+
+    public Task InitializeAsync(NavigationContext context)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task<bool> IsNavigationTargetAsync(NavigationContext context)
+    {
+        return Task.FromResult(true);
+    }
+
+    public Task OnNavigatedFromAsync(NavigationContext context)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task OnNavigatedToAsync(NavigationContext context)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task OnUnloadAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ tests/AsyncNavigation.Tests/Mocks/TestMultiAwareViewModel.cs
@@ -0,0 +1,52 @@
+﻿using AsyncNavigation.Abstractions;
+using AsyncNavigation.Core;
+
+namespace AsyncNavigation.Tests.Mocks;
+
+public class TestMultiAwareViewModel : INavigationAware, IDialogAware
+{
+    public string Title => nameof(TestMultiAwareViewModel);
+
+    public event AsyncEventHandler<AsyncEventArgs>? AsyncRequestUnloadEvent;
+    public event AsyncEventHandler<DialogCloseEventArgs>? RequestCloseAsync;
+
+    public Task InitializeAsync(NavigationContext context)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task<bool> IsNavigationTargetAsync(NavigationContext context)
+    {
+        return Task.FromResult(true);
+    }
+
+    public Task OnDialogClosedAsync(IDialogResult? dialogResult, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task OnDialogClosingAsync(IDialogResult? dialogResult, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task OnDialogOpenedAsync(IDialogParameters? parameters, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task OnNavigatedFromAsync(NavigationContext context)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task OnNavigatedToAsync(NavigationContext context)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task OnUnloadAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
