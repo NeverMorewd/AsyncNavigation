0b88d060239d39e43c1c119ca59b65df019ffda3
Dong <nevermore.wd@me.com>
2026-04-18 12:29:04 +0200

    upgrade ava to v12 (#46)
    * upgrade ava to v12
    upgrade other nuget packages
    add Central Package Management
    
    * update workflow
    
    * update workflow
    
    * porting android to v12

--- samples/Sample.Avalonia.Android/MainActivity.cs
+++ samples/Sample.Avalonia.Android/MainActivity.cs
@@ -1,26 +1,10 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Avalonia;
 using Avalonia.Android;
-using ReactiveUI.Avalonia;
 
-namespace Sample.Avalonia.Android
+namespace Sample.Avalonia.Android;
+
+[Activity(Label = "Sample.Avalonia.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", MainLauncher = true, Exported = true, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity
 {
-    [Activity(
-        Label = "Sample.Avalonia.Android",
-        Theme = "@style/MyTheme.NoActionBar",
-        Icon = "@drawable/icon",
-        MainLauncher = true,
-        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-    public class MainActivity : AvaloniaMainActivity<App>
-    {
-        protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
-        {
-            var abuilder = base.CustomizeAppBuilder(builder)
-                .WithInterFont()
-                .UseReactiveUI();
-            var app = abuilder.Instance as App;
-            return abuilder;
-        }
-    }
 }
