785cf30d9b541e20f4fe578ac7fb23021cf802f9
Easley <nevermore.wd@me.com>
2026-04-18 12:24:16 +0200

    porting android to v12

---
+++ samples/Sample.Avalonia.Android/Application.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Runtime;
+using Avalonia;
+using Avalonia.Android;
+using ReactiveUI.Avalonia;
+
+namespace Sample.Avalonia.Android;
+
+[Application]
+public class Application : AvaloniaAndroidApplication<App>
+{
+    protected Application(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+    {
+    }
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder).UseReactiveUI();
+    }
+}
